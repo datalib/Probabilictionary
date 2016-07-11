b7b7fd137a69f5b710d15e6ae2f18b41e6500ad1
im-rodrigo <rodrigopala91@gmail.com>
2016-07-10 22:03:52 -0700

    adding helper funcs and changes to class constructor

--- probabilictionary/probabilictionary.py
+++ probabilictionary/probabilictionary.py
@@ -1,8 +1,38 @@
 debug = True
 
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l)):
+        yield l[i:i + n]
+
+
+def weighted_choice(choices):
+    import random
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+
+    assert False, "Shouldn't get here"
+
+
+def normalize(freq_dist):
+    norm_const = sum(freq_dist.values())
+    return Counter({k: v / norm_const for k, v in freq_dist.items()})
+
+
+from collections import Counter
+
+
 class Probabilictionary():
+    # TODO: Rename *events* to *outcomes*
+    # TODO: Check *outcomes* type and add appropriate logic
     def __init__(self, events, ordered=False):
+        # check *events* type
         if ordered:
             from ordered_set import OrderedSet
             set_constructor = OrderedSet
@@ -11,7 +41,9 @@ class Probabilictionary():
 
         self.ordered = ordered
 
-        self.S = {outcome for event in events for outcome in event}
+        # Temporary
+        self._prob_dist = normalize(Counter([outcome for event in events
+                                             for outcome in event]))
 
         self.events = [set_constructor(event) for event in events]
 
@@ -25,6 +57,9 @@ class Probabilictionary():
 
     def __repr__(self):
         return str(self.events)
+
+    def S(self):
+        return set(self.freq_dist)
 
     def P(self, *args, **kwargs):
         if debug:
